a4628b5505d399cc37d56af60bc690884edd661a
dcrelling <david.crelling@runkeeper.com>
2016-11-13 15:49:25 -0500

    added a main method

--- Lab1/app/src/main/java/com/example/runkeeper/lab1/Main.java
+++ Lab1/app/src/main/java/com/example/runkeeper/lab1/Main.java
@@ -6,4 +6,7 @@ package com.example.runkeeper.lab1;
 
 public class Main
 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
 }
