4bd6c15a4bf0d2826836b10914e7df4656d61489
dcrelling <david.crelling@runkeeper.com>
2016-11-13 16:46:13 -0500

    adding some classes and solutions labs

---
+++ Lab1/app/src/main/java/com/example/runkeeper/lab1/Factorial.java
@@ -0,0 +1,16 @@
+package com.example.runkeeper.lab1;
+
+
+public class Factorial
+{
+
+    public long factorial(long number)
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
+    }
+}
--- Lab1/app/src/main/java/com/example/runkeeper/lab1/Main.java
+++ Lab1/app/src/main/java/com/example/runkeeper/lab1/Main.java
@@ -3,7 +3,8 @@ package com.example.runkeeper.lab1;
 
 public class Main
 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Runkeeper!");
     }
 }
